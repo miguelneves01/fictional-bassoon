bf972e2f5f894d8834a633c43b2a91eae4b16b65
Miguel <miguelcresponeves@gmail.com>
2025-02-20 20:13:05 +0000

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2">
-        <h1 className="text-4xl font-bold">Time since David's car broke down</h1>
+        <h1 className="text-4xl font-bold">Time since David&apos;s car broke down</h1>
         <div className="flex flex-row gap-4 items-center justify-center">
           <div className="flex flex-col gap-4 text-center">
             <span className="min-w-[4.5rem] text-3xl font-bold border-red-500 border-2 p-4 rounded-lg ">{days}</span>
